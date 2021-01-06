e5284e9efc8945347fdc058393f616e22337d5b0
Vincent <1075678806@qq.com>
2021-01-06 16:09:28 +0800

    test

---
+++ src/test/java/org/wink/sign_in/SignInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wink.sign_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
